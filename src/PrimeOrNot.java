84797848fcd0520bec86dc8e22b5302385a11170
piyu1234 <league@WTS10.attlocal.net>
2018-03-05 18:24:11 -0800

    PrimeOrNot, Fibonacci, GoofyNames (IP)

--- src/PrimeOrNot.java
+++ src/PrimeOrNot.java
@@ -1,4 +1,20 @@
+import javax.swing.JOptionPane;
 
 public class PrimeOrNot {
-
+	public static void main(String[] args) {
+		boolean prime = true;
+		String number = JOptionPane.showInputDialog("What number would you like to prime test?");
+		int num = Integer.parseInt(number);
+		for (int i = 2; i < num ; i++) {
+			if (num%i==0) {
+				prime = false;
+				JOptionPane.showMessageDialog(null, number + " is not a Prime Number.");
+				break;
+				
+			}
+		}
+		if (prime==true) {
+			JOptionPane.showMessageDialog(null, number + " is a Prime Number.");
+		}
+	}
 }
